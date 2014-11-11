517621807af8a64fcb20e9d1dbf6ad5aad741fa9
Oliv <olivier-marin@hotmail.fr>
2014-11-11 10:45:40 +0100

    Création du main

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  projet_algo
+//
+//  Created by Olivier on 11/11/2014.
+//  Copyright (c) 2014 Alex&Olivier. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+	cout << "On vas en chier !!" << endl;
+    return 0;
+}
+
